dcb882531ef14e89da77ac30b60ea0b01e4492ef
Guillermo B <guillermo.bulo@gmail.com>
2020-06-25 10:05:18 -0400

    Docs Agregado

---
+++ docs/precache-manifest.f2acbff578138fb9110c2a93947c7963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa2b6bff445adf862cb005317b77734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dc30aa38d9a5b714dd",
+    "url": "/static/css/main.1801688d.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65dc30aa38d9a5b714dd",
+    "url": "/static/js/main.9bbad71e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
